a181b2066c132d24b20ec15d878ea7c2ab336baa
AirplaneBot <bot-github@airplane.dev>
2021-07-30 11:18:27 -0400

    Brew formula update for airplane version v0.1.20

--- airplane.rb
+++ airplane.rb
@@ -5,28 +5,28 @@
 class Airplane < Formula
   desc ""
   homepage "https://airplane.dev"
-  version "0.1.19"
+  version "0.1.20"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/airplanedev/cli/releases/download/v0.1.19/airplane_darwin_x86_64.tar.gz"
-      sha256 "f77800a8f4bb345ca916a43b06bad0510f51fe694e5af4965d10799ce4d9f101"
+      url "https://github.com/airplanedev/cli/releases/download/v0.1.20/airplane_darwin_x86_64.tar.gz"
+      sha256 "e86a2e6ebfa9ea62184ae901f9f3c093f4bbbb6375979fe7319f63855f9b230b"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/airplanedev/cli/releases/download/v0.1.19/airplane_darwin_arm64.tar.gz"
-      sha256 "574d54c999b385c536e29da88f5bdf46df846d2b2ae7d8564ed020ae1d129ecc"
+      url "https://github.com/airplanedev/cli/releases/download/v0.1.20/airplane_darwin_arm64.tar.gz"
+      sha256 "33a40de104c23d820d3438ad3bba8a33067b3447eebe2cb3d8607a99d6752e0a"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/airplanedev/cli/releases/download/v0.1.19/airplane_linux_x86_64.tar.gz"
-      sha256 "bdb46cb2413e05e67b399a32eaafc160258ff737c17351856db371079c7b58ef"
+      url "https://github.com/airplanedev/cli/releases/download/v0.1.20/airplane_linux_x86_64.tar.gz"
+      sha256 "462145603db2b087b05c5abd8673c1ec1f781f55d96b35401fd81e663be979bc"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/airplanedev/cli/releases/download/v0.1.19/airplane_linux_arm64.tar.gz"
-      sha256 "363d1049429bf16ea51df242cb80ab8e9bd2712bf2b8718f20a1dd45d8b2b24d"
+      url "https://github.com/airplanedev/cli/releases/download/v0.1.20/airplane_linux_arm64.tar.gz"
+      sha256 "d9c3a042893eb70861a16976c52d49ac305312853a82ad4c9c50f1efb70ba0ea"
     end
   end
 
